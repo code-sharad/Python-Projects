f40e98220cc157fd6b295dac27d7fada88376937
Nikhil Bhadait <codesharad@gmail.com>
2021-01-17 19:22:01 +0530

    Added folder called Quiz day17

---
+++ Quiz day17/main.py
@@ -0,0 +1,8 @@
+question_date = [
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "The loudest animal is the African elephant.", "answer": "False"},
+    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+    {"text": "In West Virginia, USA if you accidentally hit an animal with your car, you are free to take it home to" 
+             "eat.", "answer": "True"},
+]
